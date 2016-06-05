3b16e3cb14f297e8cf072e062228afbc89b1a5ee
Zocklukas <zocklukas04@gmail.com>
2016-06-05 02:14:13 +0200

    Add files via upload

--- SpiritMod/Items/Spirit/Tiles/SpiritWorkbench.cs
+++ SpiritMod/Items/Spirit/Tiles/SpiritWorkbench.cs
@@ -1,32 +1,36 @@
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ObjectData;
 
-namespace SpiritMod.Items.Spirit
+namespace SpiritMod.Items.Spirit.Tiles
 {
-	public class SpiritWorkbench : ModItem
+	public class SpiritWorkbench : ModTile
 	{
 		public override void SetDefaults()
 		{
-			item.name = "Spirit Workbench";
-            item.width = 44;
-			item.height = 25;
-			item.maxStack = 99;
-			item.useTurn = true;
-			item.autoReuse = true;
-			item.useAnimation = 15;
-			item.useTime = 10;
-			item.useStyle = 1;
-			item.consumable = true;
-			item.value = 150;
-			item.createTile = mod.TileType("SpiritWorkbench");
+			Main.tileSolidTop[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileTable[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
+			TileObjectData.addTile(Type);
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+			AddMapEntry(new Color(30, 144, 255), "Spirit Workbench");
+			disableSmartCursor = true;
+			adjTiles = new int[]{ TileID.WorkBenches };
 		}
 
-		public override void AddRecipes()
+		public override void NumDust(int i, int j, bool fail, ref int num)
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(null,"SpiritWoodItem", 10);
-            recipe.SetResult(this);
-			recipe.AddRecipe();            
+			num = fail ? 1 : 3;
+		}
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("SpiritWorkbench"));
         }
-	}
+    }
 }
