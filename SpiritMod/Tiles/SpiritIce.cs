220acd1c69c56365ab68cdbc6dbc5d385ed987f1
Zocklukas <zocklukas04@gmail.com>
2016-06-08 15:35:21 +0200

    Update SpiritIce.cs

--- SpiritMod/Tiles/SpiritIce.cs
+++ SpiritMod/Tiles/SpiritIce.cs
@@ -16,7 +16,7 @@ namespace SpiritMod.Tiles
 			Main.tileMergeDirt[Type] = true;
 			Main.tileBlockLight[Type] = true;
 			Main.tileLighted[Type] = true;
-			AddMapEntry(new Color(128, 128, 128));
+			AddMapEntry(new Color(70, 130, 180));
 		//	drop = mod.ItemType("SpiritIceItem");
 		}
 
