0dd9e513bf710b3261235bc687c7b5ee461b0a3e
Graydee <wiseguy0002@gmail.com>
2016-06-15 18:09:17 -0400

    FINALLY i completed phase 2 AI

--- SpiritMod/NPCs/Bosses/IlluminantMaster.cs
+++ SpiritMod/NPCs/Bosses/IlluminantMaster.cs
@@ -6,6 +6,8 @@ namespace SpiritMod.NPCs.Bosses
 {
     public class IlluminantMaster : ModNPC
     {
+		private float XSpeed;
+		private float YSpeed;
         public override void SetDefaults()
         {
             npc.name = "Illuminant Master";
@@ -26,28 +28,33 @@ namespace SpiritMod.NPCs.Bosses
         {
 		if (npc.life < 11000)
 		{ 
-			int Xdis = Main.player[Main.myPlayer].Center.X - npc.Center.X;  // change myplayer to nearest player in full version
-			int Ydis = Main.player[Main.myPlayer].Center.Y - npc.Center.Y; // change myplayer to nearest player in full version
-			float Hypotanuse = (float)Math.Sqrt((Xdis * Xdis) + (Ydis * Ydis));
-			float TrijectoryX = (float)Math.Acos(Xdis / Hypotanuse) * 3;
-			float TrijectoryY = (float)Math.Asin(Ydis / Hypotanuse) * 3;
-			npc.ai[0]++;
-			if(npc.ai[0] % 250 >= 75) // X
-			{
-				npc.speedX = TrijectoryX * (175 / npc.ai[0] % 200);
-				npc.speedY = TrijectoryY * (175 / npc.ai[0] % 200);
-			}
-			if(npc.ai[0] % 250 == 0) // Y
-			{
-				npc.position.X = (Main.player[Main.myPlayer].position.X - 250) + Main.rand.Next(500);
-				npc.position.Y = (Main.player[Main.myPlayer].position.Y - 250) + Main.rand.Next(500);
-			}
-			if(npc.ai[0] % 250 <= 75) // Z
-			{
-				npc.speedX = TrijectoryX / 20;
-				npc.speedY = TrijectoryX / 20;
-			}
-			if (npc.ai[0]%8==0)
+			float Xdis = Main.player[Main.myPlayer].Center.X - npc.Center.X;  // change myplayer to nearest player in full version
+            float Ydis = Main.player[Main.myPlayer].Center.Y - npc.Center.Y; // change myplayer to nearest player in full version
+            float Angle = (float)Math.Atan(Xdis / Ydis);
+            float TrijectoryX = (float)(Math.Sin(Angle));
+            float TrijectoryY = (float)(Math.Cos(Angle));
+            npc.ai[0]++;
+           if(npc.ai[0] % 250 <= 75) // X
+            {
+                XSpeed = TrijectoryX;
+                YSpeed = TrijectoryY;
+            }
+            if(npc.ai[0] % 250 > 75) // X
+            {
+                npc.velocity.X = XSpeed;
+                npc.velocity.Y = YSpeed;
+            }
+            if(npc.ai[0] % 250 == 0) // Y
+            {
+                npc.position.X = (Main.player[Main.myPlayer].position.X - 125) + Main.rand.Next(250);
+                npc.position.Y = (Main.player[Main.myPlayer].position.Y - 125) + Main.rand.Next(250);
+            }
+            if(npc.ai[0] % 250 < 75) // Z
+            {
+                npc.velocity.X = 0;
+                npc.velocity.Y = 0;
+            }
+            if (npc.ai[0]%8==0)
             {
                 npc.frame.Y = (int)(npc.height * npc.frameCounter);
                 npc.frameCounter = (npc.frameCounter+1) % 5;
