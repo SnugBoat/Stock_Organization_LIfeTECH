aa905c348dd80df9c977c73f483a65d048dee33c
EnderSho355 <EnderShot355@gmail.com>
2016-06-27 10:13:22 -0400

    Added new SpiritMod CS

---
+++ SpiritMod/SpiritMod.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod
+{
+    class SpiritMod : Mod
+    {
+        public SpiritMod()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
