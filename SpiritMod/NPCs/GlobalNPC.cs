3300b269c71ae3de1b2e8fb741099f2b2201b776
TMC <cgamez995@gmail.com>
2016-06-27 16:15:06 -0700

    Update GlobalNPC.cs

--- SpiritMod/NPCs/GlobalNPC.cs
+++ SpiritMod/NPCs/GlobalNPC.cs
@@ -11,15 +11,31 @@ namespace SpiritMod.NPCs
 		public override void ResetEffects(NPC npc)
 		{
 			GlobalNPCInfo info = (GlobalNPCInfo)npc.GetModInfo(mod, "GlobalNPCInfo");
-			info.eFlames = false;
+			info.DoomDestiny = false;
 		}
 		
 		public override void NPCLoot(NPC npc)
 		{
 		  if (npc.type == NPCID.Plantera)
 			{
-				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("ThornBloomKnife"), Main.rand.Next(40, 60));
+				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("ThornbloomKnife"), Main.rand.Next(40, 60));
 			}
 		}
-	}
+
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (npc.GetModInfo<GlobalNPCInfo>(mod).DoomDestiny)
+            {
+                if (npc.lifeRegen > 0)
+                {
+                    npc.lifeRegen = 0;
+                }
+                npc.lifeRegen -= 16;
+                if (damage < 10)
+                {
+                    damage = 10;
+                }
+            }
+        }
+    }
 }
