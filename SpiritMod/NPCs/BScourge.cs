8ed1363656d59f72cfe50b150d31ff9c2461a7ca
PhoenixBladez <aryanandabhay@gmail.com>
2016-06-27 07:33:17 -0700

    Added new NPCs (enemies, 3) updated 1 sprite

---
+++ SpiritMod/NPCs/BScourge.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.NPCs
+{
+    public class BScourge : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Bubonic Scourge";
+            npc.displayName = "Bubonic Scourge";
+            npc.width = 64;
+            npc.height = 36;
+            npc.damage = 85;
+            npc.defense = 28;
+            npc.lifeMax = 700;
+            npc.soundHit = 1;
+            npc.soundKilled = 1;
+            npc.value = 60f;
+            npc.knockBackResist = 0f;
+            npc.aiStyle = 26;
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.NebulaBeast];
+            aiType = NPCID.Unicorn;
+            animationType = NPCID.NebulaBeast;
+        }
+
+        public override float CanSpawn(NPCSpawnInfo spawnInfo)
+        {
+            return spawnInfo.spawnTileY < Main.rockLayer ? 0.5f : 0f;
+        }
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            for (int i = 0; i < 10; i++) ;
+        }
+    }
+}
