a485d487f7a15ad4a47130c252d4cbbe72e29f76
PhoenixBladez <aryanandabhay@gmail.com>
2016-07-01 20:28:13 +0530

    Added/Update enemies

---
+++ SpiritMod/NPCs/CoreB.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.NPCs
+{
+    public class CoreB : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Core Bat";
+            npc.displayName = "Core Bat";
+            npc.width = 20;
+            npc.height = 12;
+            npc.damage = 19;
+            npc.defense = 11;
+            npc.lifeMax = 90;
+            npc.soundHit = 1;
+            npc.soundKilled = 4;
+            npc.value = 60f;
+            npc.knockBackResist = .72f;
+            npc.aiStyle = 14;
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.CaveBat];
+            aiType = NPCID.CaveBat;
+            animationType = NPCID.CaveBat;
+        }
+
+        public override float CanSpawn(NPCSpawnInfo spawnInfo)
+        {
+            return spawnInfo.spawnTileY < Main.rockLayer ? 0.5f : 0f;
+        }
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            for (int i = 0; i < 10; i++) ;
+        }
+    }
+}
