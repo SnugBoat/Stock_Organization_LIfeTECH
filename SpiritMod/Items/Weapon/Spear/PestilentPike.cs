e97e706514b5af676845418581e6ac5285c96963
ColinAV516 <colinav516@gmail.com>
2016-07-07 16:58:02 -0400

    Added Pestilent Pike

---
+++ SpiritMod/Items/Weapon/Spear/PestilentPike.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Weapon.Spear {
+public class PestilentPike : ModItem
+{
+    public override void SetDefaults()
+    {
+        item.name = "Pestilent Pike";
+        item.useStyle = 5;
+        item.width = 24;
+        item.height = 24;
+        item.noUseGraphic = true;
+        item.useSound = 1;
+        item.melee = true;
+        item.noMelee = true;
+        item.useAnimation = 27;
+        item.useTime = 27;
+        item.shootSpeed = 5f;
+        item.knockBack = 6f;
+        item.damage = 77;
+        item.value = Item.sellPrice(0, 1, 30, 0);
+        item.rare = 3;
+        item.shoot = mod.ProjectileType("PestilentPikeProj");
+    }
+}}
