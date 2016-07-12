4f2751cd4e24f6563c8637c8eba5968562b3014d
ColinAV516 <colinav516@gmail.com>
2016-07-12 13:26:25 -0400

    Now inflicts blighted flames

--- SpiritMod/Projectiles/Thrown/PestilantShurikenProjectile.cs
+++ SpiritMod/Projectiles/Thrown/PestilantShurikenProjectile.cs
@@ -44,7 +44,7 @@ public class PestilantShurikenProjectile : ModProjectile
         {
             if (Main.rand.Next(2) == 0)
             {
-                target.AddBuff(BuffID.CursedInferno, 60, false);
+                target.AddBuff(mod.BuffType("BlightedFlames"), 60, false);
             }
             			Player player = Main.player[projectile.owner];
 			((MyPlayer)player.GetModPlayer(mod, "MyPlayer")).PutridHits++;
