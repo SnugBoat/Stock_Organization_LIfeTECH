6c31810477ed9a1aa17743f91fece431d0eec968
PhoenixBladez <aryanandabhay@gmail.com>
2016-07-22 01:28:24 +0530

    Added Blood Corruption Debuff

---
+++ SpiritMod/Buffs/BCorrupt.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Buffs
+{
+    public class BCorrupt : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.buffName[this.Type] = "Blood Corruption";
+        }
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (npc.lifeRegen > 0)
+                npc.lifeRegen = 0;
+            npc.lifeRegen -= 4;
+            {
+                int dust = Dust.NewDust(npc.position, npc.width, npc.height, 5);
+            }
+        }
+    }
+}
