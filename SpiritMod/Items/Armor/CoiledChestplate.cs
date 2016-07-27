c8ec23bb23dd9337e5e61fbef268a50afd191c87
PhoenixBladez <aryanandabhay@gmail.com>
2016-07-27 09:58:28 +0530

    Added Coiled Armor

---
+++ SpiritMod/Items/Armor/CoiledChestplate.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Armor
+{
+    public class CoiledChestplate : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            equips.Add(EquipType.Body);
+            return true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.name = "Coiled Chestplate";
+            item.width = 30;
+            item.height = 20;
+            AddTooltip("Increases Critical Strike Chance by 8%");
+            item.value = 22000;
+            item.rare = 1;
+            item.defense = 4;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.magicCrit += 8;
+            player.meleeCrit += 8;
+            player.thrownCrit += 8;
+            player.rangedCrit += 8;
+        }
+    }
+}
